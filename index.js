59366170e762bf08b73d0839ecda56f4a2a3104c
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-17 08:57:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,18 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+
 /* Write your implementation of greet() */
+function greet(timeString){
+  if(timeString=="8:40"){
+    return Good Morning;
+  }
+else(timeString=="14:21"){
+    return Good Afternoon;
+  }
+if(timeString=="19:00"){
+    return 'Good Evening';
+  }
+
+}
 /* Write your implementation of displayMessage() */
