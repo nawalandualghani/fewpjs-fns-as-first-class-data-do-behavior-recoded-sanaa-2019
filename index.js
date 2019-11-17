c7f568ee3168dc38f8797256d41bde94356b01a6
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-17 09:44:02 +0000

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,33 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+
 /* Write your implementation of greet() */
+function greet(timeString){
+  if(timeString<"12pm"){
+    // console.log("Good Morning");
+    return "Good Morning";
+  }
+if(timeString>"12pm" && timeString<"5pm"){
+      // console.log("Good Afternoon");
+      return "Good Afternoon";
+  }
+if(timeString>"5pm"){
+      // console.log("Good Evening");
+      return "Good Evening";
+  }
+
+}
+greet();
 /* Write your implementation of displayMessage() */
+
+function displayMessage(timeString){
+  if (greet(timeString)==="") {
+    let testContent = "TEST"
+  let content = document.getElementById("greeting");
+  content.innerText="testContent";
+  }
+
+
+}
+displayMessage();
